ce0021ea6b5679f99fe489fe37f99d143184819a
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2017-08-15 23:21:02 +0100

    Updated to 3.0.0-ALPHA7.

--- src/SkyBlock/generator/generators/BasicIsland.php
+++ src/SkyBlock/generator/generators/BasicIsland.php
@@ -58,7 +58,7 @@ class BasicIsland extends SkyBlockGenerator {
         return $this->settings;
     }
 
-    public function generateChunk($chunkX, $chunkZ) {
+    public function generateChunk(int $chunkX, int $chunkZ) {
         $chunk = $this->level->getChunk($chunkX, $chunkZ);
         $chunk->setGenerated();
         if ($chunkX % 20 == 0 && $chunkZ % 20 == 0) {
@@ -79,7 +79,7 @@ class BasicIsland extends SkyBlockGenerator {
         }
     }
 
-    public function populateChunk($chunkX, $chunkZ) {
+    public function populateChunk(int $chunkX, int $chunkZ) {
         //TODO: Set Biome ID?
         return;
     }
